03c766841ab906995481834812fa8c6a393642b3
Jason Koceja <jason@koceja.com>
2020-10-14 15:02:18 -0700

    adds IBOulets stubs for storyboard

--- TypeRacer/Controllers/ViewControllers/ViewController.swift
+++ TypeRacer/Controllers/ViewControllers/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
+/*
+ reset button
+ quit button
+ words to type label
+ clocklabel
+ type entry text field
+ wordsperminutelabel
+ */
+
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var quitButton: UIButton!
+    @IBOutlet weak var typeEntryTextField: UITextField!
+    @IBOutlet weak var clockLabel: UILabel!
+    @IBOutlet weak var wordsPerMinuteLabel: UILabel!
+    @IBOutlet weak var paragraphLabel: UILabel!
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: - Helpers
 
 }
 
