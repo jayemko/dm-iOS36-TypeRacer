85da006a2cc13619250b6eb83f70cddc05bba490
JordanBryant34 <jordanbryant334@gmail.com>
2020-10-14 15:43:59 -0700

    Implemented createTypeRacer and completeTypeRacer functions

--- TypeRacer/Controllers/ModelControllers/TypeRacerController.swift
+++ TypeRacer/Controllers/ModelControllers/TypeRacerController.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class TypeRacerController {
+    
+    static func createTypeRacer(paragraph: String, username: String, timeCompleted: TimeInterval? = nil, wpm: Int? = nil) -> TypeRacer {
+        return TypeRacer(paragraph: paragraph, username: username, timeCompleted: timeCompleted, wpm: wpm)
+    }
+    
+    static func completeTypeRacer(typeRacer: TypeRacer, timeCompleted: TimeInterval) {
+        typeRacer.timeCompleted = timeCompleted
+        
+        let words = typeRacer.paragraph.components(separatedBy: " ")
+        let wpm = words.count / (Int(timeCompleted) / 60)
+        
+        typeRacer.wpm = wpm
+    }
+    
+}
