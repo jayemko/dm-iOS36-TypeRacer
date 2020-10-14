0e18eabf62b9a717cafe402ccf131d1c4cf2c5c5
JordanBryant34 <jordanbryant334@gmail.com>
2020-10-14 13:16:22 -0700

    added paragraphs file
    (cherry picked from commit 210dc3ee73c1b4db43e48c1a6309f16d1ac4d51c)

---
+++ TypeRacer/Paragraphs.swift
@@ -0,0 +1,30 @@
+//
+//  Paragraphs.swift
+//  TypeRacer
+//
+//  Created by Jordan Bryant on 10/14/20.
+//
+
+import Foundation
+
+class Paragraphs {
+    let textPrompts: [String] = [
+        
+        "Web designers are expected to have an awareness of usability and if their role involves creating mark up then they are also expected to be up to date with web accessibility guidelines. The different areas of web design include web graphic design; interface design; authoring, including standardised code and proprietary software; user experience design; and search engine optimization.",
+        
+        "Your ring finger is the most difficult of all fingers to control. You will notice that when you move your ring finger your other fingers will move involuntarily as well. This is something that will become less of a problem as you perfect your keyboarding skills.",
+        
+        "Words per minute is a measure of typing speed, commonly used in recruitment. For the purposes of WPM measurement a word is standardized to five characters or keystrokes. Therefore, \"brown\" counts as one word, but \"accounted\" counts as two. The benefits of a standardized measurement of input speed are that it enables comparison across language and hardware boundaries.",
+        
+        "Closed captions were created for deaf or hard of hearing individuals to assist in comprehension. They can also be used as a tool by those learning to read, learning to speak a non-native language, or in an environment where the audio is difficult to hear or is intentionally muted.",
+        
+        "A virtual assistant (typically abbreviated to VA) is generally self-employed and provides professional administrative, technical, or creative assistance to clients remotely from a home office.",
+        
+        "A transcription service is a business which converts speech into a written or electronic text document. Transcription services are often provided for business, legal, or medical purposes. The most common type of transcription is from a spoken-language source into text such as a computer file suitable for printing as a document such as a report. Common examples are the proceedings of a court hearing such as a criminal trial (by a court reporter) or a physician's recorded voice notes.",
+        
+        "Business casual is an ambiguously defined dress code that has been adopted by many professional and white-collar workplaces in Western countries. It entails neat yet casual attire and is generally more casual than informal attire but more formal than casual or smart casual attire. Casual Fridays preceded widespread acceptance of business casual attire in many offices.",
+        
+        "A paralegal is a person trained in legal matters who performs tasks requiring knowledge of the law and legal procedures. A paralegal is not a lawyer but can be employed by a law office or work freelance at a company or law office. Paralegals are not allowed to offer legal services directly to the public on their own and must perform their legal work under an attorney or law firm."
+        
+    ]
+}
