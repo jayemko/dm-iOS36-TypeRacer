cea7c42591533ef42cd6461c82ad5429898db423
Jason Koceja <jason@koceja.com>
2020-10-14 15:03:11 -0700

    removes comments

--- TypeRacer/Controllers/ViewControllers/ViewController.swift
+++ TypeRacer/Controllers/ViewControllers/ViewController.swift
@@ -7,15 +7,6 @@
 
 import UIKit
 
-/*
- reset button
- quit button
- words to type label
- clocklabel
- type entry text field
- wordsperminutelabel
- */
-
 class ViewController: UIViewController {
     
     // MARK: - Outlets
@@ -29,13 +20,10 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     
-    
-    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     // MARK: - Helpers
