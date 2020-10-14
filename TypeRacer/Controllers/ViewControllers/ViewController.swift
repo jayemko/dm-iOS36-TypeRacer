571391baa09084070de550435361aff928ee8f32
Jason Koceja <jason@koceja.com>
2020-10-14 11:18:34 -0700

    relocates storyboard to folder

---
+++ TypeRacer/Controllers/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TypeRacer
+//
+//  Created by Jason Koceja on 10/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
