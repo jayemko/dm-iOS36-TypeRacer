104f6c3dc89f2723d54b7c74f9076bea51aa86fe
Jason Koceja <jason@koceja.com>
2020-10-15 11:21:32 -0700

    adds view setup

--- TypeRacer/Controllers/ViewControllers/StartViewController.swift
+++ TypeRacer/Controllers/ViewControllers/StartViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class StartViewController : UIViewController {
     
+    // MARK: - Outlets
     @IBOutlet weak var startButton: UIButton!
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    // touch up inside
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        // start new game
+    }
+    
+    // MARK: - Helpers
+    
+    func createNewGame() -> TypeRacer {
+        let newTypeRacer = TypeRacerController.createTypeRacer()
+        return newTypeRacer
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ("toGame") {
+            guard let destination = segue.destination as? ViewController else { return }
+            let newGame = createNewGame()
+            destination.game = newGame
+        }
     }
 }
